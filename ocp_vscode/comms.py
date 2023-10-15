194165a81b8c515d8c1b016b29d7786b460ace3f
Bernhard <b_walter@arcor.de>
2023-10-15 17:44:26 +0200

    add size of payload to timeit message

--- ocp_vscode/comms.py
+++ ocp_vscode/comms.py
@@ -57,7 +57,7 @@ def _send(data, message_type, port=None, timeit=False):
             elif message_type == MessageType.backend_response:
                 j = b"R:" + j
 
-        with Timer(timeit, "", "websocket send", 1):
+        with Timer(timeit, "", f"websocket send {len(j)/1024/1024:.3f} MB", 1):
             ws = connect(f"{CMD_URL}:{port}")
             ws.send(j)
 
