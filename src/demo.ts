b573bfa05ab8627a842c27d484c97b58c72a59f6
Bernhard <b_walter@arcor.de>
2023-12-10 16:13:08 +0100

    --amend

--- src/demo.ts
+++ src/demo.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as os from 'os';
+import { getCurrentFolder } from "./utils";
 
 const build123d_demo = `# %%
 
