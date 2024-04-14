ea1d285d0188aed6bcc7eb5680f1590e09c74af3
Bernhard <b_walter@arcor.de>
2024-04-14 18:53:42 +0200

    add newest ocp-tessellate

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup_args = {
     "include_package_data": True,
     "python_requires": ">=3.9",
     "install_requires": [
-        "ocp-tessellate>=2.2.2,<2.3.0",
+        "ocp-tessellate>=2.3.0,<2.4.0",
         "requests",
         "ipykernel",
         "orjson",
