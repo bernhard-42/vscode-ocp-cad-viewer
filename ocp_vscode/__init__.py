337da5473550d7908a87cd3ac1db23f5cb203bdb
Bernhard <b_walter@arcor.de>
2024-04-14 18:55:47 +0200

    bump version

--- ocp_vscode/__init__.py
+++ ocp_vscode/__init__.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-__version__ = "2.2.2"
+__version__ = "2.3.0"
 
 from .show import *
 from .config import *
