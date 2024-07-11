f2444034c489592f7e31235b103daeffa84299f6
Bernhard <b_walter@arcor.de>
2024-07-11 21:41:30 +0200

    remove old PartGroup

--- ocp_vscode/show.py
+++ ocp_vscode/show.py
@@ -20,7 +20,7 @@ import os
 import re
 import types
 
-from ocp_tessellate import PartGroup
+from ocp_tessellate import OcpGroup
 from ocp_tessellate.convert import (
     tessellate_group,
     get_normal_len,
@@ -166,9 +166,7 @@ def _tessellate(
             progress=progress,
         )
 
-        if len(part_group.objects) == 1 and isinstance(
-            part_group.objects[0], PartGroup
-        ):
+        if len(part_group.objects) == 1 and isinstance(part_group.objects[0], OcpGroup):
             part_group = part_group.objects[0]
 
     params = {
