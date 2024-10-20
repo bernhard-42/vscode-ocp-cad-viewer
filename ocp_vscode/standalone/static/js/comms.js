0b61699bc78f553d1ac0aeaa1d91d2e06d01ae54
Bernhard <b_walter@arcor.de>
2024-10-20 14:04:29 +0200

    import of javascript modules worls

--- ocp_vscode/standalone/static/js/comms.js
+++ ocp_vscode/standalone/static/js/comms.js
@@ -1,3 +1,56 @@
-export function postMessage(message) {
-    window.parent.postMessage(message, '*');
+function handleMessage(message) {
+  // Handle the received message here
+  console.log("Handling message:", message);
+  // Add your custom logic to process the message
 }
+
+function connect() {
+  const socket = new WebSocket("ws://127.0.0.1:3939");
+
+  socket.onopen = function (event) {
+    console.log("WebSocket connection established");
+    sendListening();
+  };
+
+  socket.onmessage = function (event) {
+    console.log("Message received from server:", event.data);
+    handleMessage(event.data);
+    sendListening();
+  };
+
+  socket.onerror = function (error) {
+    console.error("WebSocket error:", error);
+  };
+
+  socket.onclose = function (event) {
+    console.log("WebSocket connection closed");
+  };
+
+  function sendListening() {
+    socket.send('L:"listening"');
+  }
+  return socket;
+}
+
+export { connect };
+
+// export function postMessage(message) {
+//     window.parent.postMessage(message, '*');
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     window.addEventListener("message", function (event) {
+//         console.log("Message received from:", event.data);
+//     });
+
+//     const socket = new WebSocket("ws://" + location.host + "/");
+
+//     socket.addEventListener("message", (ev) => {
+//         console.log("<<< " + ev.data, "blue");
+//     });
+
+//     socket.onopen = function () {
+//         console.log("Connection established", "green");
+//         socket.send("Hello from js");
+//     };
+// };
