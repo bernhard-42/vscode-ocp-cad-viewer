46a16b5d83dd06930e5031fc3094b6a46da6ea85
Bernhard <b_walter@arcor.de>
2025-04-04 17:56:36 +0200

    exclude Jupyter _# variables

--- ocp_vscode/show.py
+++ ocp_vscode/show.py
@@ -848,6 +848,7 @@ def show_all(
             or name in exclude + ["_", "__", "___", "_ih", "_oh", "_dh", "Out", "In"]
             or name.startswith("__")
             or re.search("^_i\\d+", name) is not None
+            or re.search("^_\\d+", name) is not None
             # pylint: disable=protected-access
             or (hasattr(obj, "_obj") and obj._obj is None)
             or callable(obj)
