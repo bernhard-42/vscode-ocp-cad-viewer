8a362b8cde4c97dc0bb3fc409a0999d657b31338
Bernhard <b_walter@arcor.de>
2026-04-04 17:11:25 +0200

    fix material class

--- examples/toy-rider.py
+++ examples/toy-rider.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from build123d import *
 from ocp_vscode import *
-from threejs_materials import Material
+from threejs_materials import PbrProperties
 
 try:
     model = import_step(Path.home() / "Downloads" / "Toy Rider S2022 ASM stp.STEP")
@@ -14,38 +14,25 @@ show(Rot(90, 0, 0) * model)
 # %%
 
 #
-# Material definition
+# PbrProperties definition
 #
 
-car_red = Material.gpuopen.load("Car Paint").override(color=(0.5, 0, 0))
-chrome = Material.gpuopen.load("Chrome")
-rubber = Material.gpuopen.load("Rubber").override(color=(0.06, 0.06, 0.06))
-glass = Material.gpuopen.load("Glass")
-steel = Material.gpuopen.load("Stainless Steel Brushed")
-fabric = Material.gpuopen.load("Midnight Blue Heavy Fabric")
-alu = Material.gpuopen.load("Aluminum Brushed")
-alu_matte = Material.gpuopen.load("Aluminum Matte").scale(2, 2)
-leather = Material.gpuopen.load("TH: Brown Leather")
-wood = Material.gpuopen.load("Mahogany Varnished").scale(2, 2)
-plastic = Material.ambientcg.load("Plastic 012 B")
-acrylic_white = Material.physicallybased.load("Plastic (Acrylic)")
-acrylic_red = Material.physicallybased.load("Plastic (Acrylic)").override(color="red")
+car_red = PbrProperties.from_gpuopen("Car Paint").override(color=(0.5, 0, 0))
+chrome = PbrProperties.from_gpuopen("Chrome")
+rubber = PbrProperties.from_gpuopen("Rubber").override(color=(0.06, 0.06, 0.06))
+glass = PbrProperties.from_gpuopen("Glass")
+steel = PbrProperties.from_gpuopen("Stainless Steel Brushed")
+fabric = PbrProperties.from_gpuopen("Midnight Blue Heavy Fabric")
+alu = PbrProperties.from_gpuopen("Aluminum Brushed")
+alu_matte = PbrProperties.from_gpuopen("Aluminum Matte").scale(2, 2)
+leather = PbrProperties.from_gpuopen("TH: Brown Leather")
+wood = PbrProperties.from_gpuopen("Mahogany Varnished").scale(2, 2)
+plastic = PbrProperties.from_ambientcg("Plastic 012 B")
+acrylic_white = PbrProperties.from_physicallybased("Plastic (Acrylic)")
+acrylic_red = PbrProperties.from_physicallybased("Plastic (Acrylic)").override(
+    color="red"
+)
 
-material_definitions = {
-    "car-red": car_red,
-    "chrome": chrome,
-    "rubber": rubber,
-    "glass": glass,
-    "steel": steel,
-    "alu": alu,
-    "alu-matte": alu_matte,
-    "leather": leather,
-    "wood": wood,
-    "fabric": fabric,
-    "plastic": plastic,
-    "acrylic_white": acrylic_white,
-    "acrylic_red": acrylic_red,
-}
 
 # %%
 
@@ -54,34 +41,34 @@ material_definitions = {
 #
 
 
-def convert(model, material_definitions):
+def convert(model):
     color_mapping = {
-        "RiderToy": "car-red",
-        "Trim": "car-red",
-        "Hood": "car-red",
-        "Door": "car-red",
-        "Cover": "plastic",
-        "Axle": "steel",
-        "Supportin": "steel",
-        "FloorBoards": "steel",
-        "Trunk_Storage": "steel",
-        "Grill": "chrome",
-        "Bumper": "chrome",
-        "Gear": "chrome",
-        "Peddle": "chrome",
-        "Bucket": "leather",
-        "DashandConsole": "wood",
-        "Floor": "fabric",
-        "Radio": "plastic",
-        "Visor": "plastic",
-        "Head_Lamp": "acrylic_white",
-        "TailLamp": "acrylic_red",
-        "Streeing": "leather",
+        "RiderToy": car_red,
+        "Trim": car_red,
+        "Hood": car_red,
+        "Door": car_red,
+        "Cover": plastic,
+        "Axle": steel,
+        "Supportin": steel,
+        "FloorBoards": steel,
+        "Trunk_Storage": steel,
+        "Grill": chrome,
+        "Bumper": chrome,
+        "Gear": chrome,
+        "Peddle": chrome,
+        "Bucket": leather,
+        "DashandConsole": wood,
+        "Floor": fabric,
+        "Radio": plastic,
+        "Visor": plastic,
+        "Head_Lamp": acrylic_white,
+        "TailLamp": acrylic_red,
+        "Streeing": leather,
     }
 
     def assign_material(obj, material):
         obj.material = material
-        obj.color = material_definitions[material].interpolate_color()
+        obj.color = material.interpolate_color()
 
     def walk(obj, ind=""):
         if hasattr(obj, "children") and obj.children:
@@ -99,15 +86,15 @@ def convert(model, material_definitions):
         else:
             if "wheel" in obj.label:
                 objects = list(obj)
-                assign_material(objects[0], "chrome")
-                assign_material(objects[1], "rubber")
+                assign_material(objects[0], chrome)
+                assign_material(objects[1], rubber)
                 obj = Compound(label=obj.label, children=objects)
             elif "Windshield" in obj.label:
                 objects = list(obj)
                 objects[0].label = "WindShield"
                 objects[1].label = "Frame"
-                assign_material(objects[0], "glass")
-                assign_material(objects[1], "chrome")
+                assign_material(objects[0], glass)
+                assign_material(objects[1], chrome)
                 obj = Compound(label=obj.label, children=objects)
             elif "WindShield" in obj.label:
                 return None
@@ -120,8 +107,5 @@ def convert(model, material_definitions):
     return walk(model)
 
 
-model2 = Rot(90, 0, 0) * convert(model, material_definitions)
-show(
-    model2,
-    material_definitions=material_definitions,
-)
+model2 = Rot(90, 0, 0) * convert(model)
+show(model2)
