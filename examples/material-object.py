b73e799d2111fdfbfd196f41573ad30f4c0e6034
Bernhard <b_walter@arcor.de>
2026-04-28 13:09:25 +0200

    small improvements

--- examples/material-object.py
+++ examples/material-object.py
@@ -25,16 +25,16 @@ materials = {
     "car_green": car_base.override(color=(0, 0.5, 0)),
     "bronze": PbrProperties.from_gpuopen("Bronze Oxydized"),
     "chrome": PbrProperties.from_gpuopen("Chrome"),
-    "glass": PbrProperties.from_gpuopen("Glass"),
-    # "red_wine": PbrProperties.from_gpuopen("Red Wine"),
-    "white_wine": PbrProperties.from_gpuopen("White Wine"),
+    "glass": PbrProperties.from_gpuopen("Glass").override(thickness=10),
+    "white_wine": PbrProperties.from_gpuopen("White Wine").override(thickness=100),
     "gold": PbrProperties.from_gpuopen("Gold"),
     "carbon_coat": PbrProperties.from_gpuopen("Carbon biColor Coat").scale(2, 2),
-    "acryl_gp": PbrProperties.from_gpuopen("Acryl Plastic").override(color=(0.5, 0, 0)),
-    "steel": PbrProperties.from_gpuopen("Stainless Steel Brushed"),
+    "acryl": PbrProperties.from_physicallybased("Plastic (Acrylic)").override(
+        color=(0.5, 0.0, 0.0), thickness=10
+    ),
+    "steel": PbrProperties.from_gpuopen("Stainless Steel Brushed").scale(3, 3),
     "alu": PbrProperties.from_gpuopen("Aluminum Brushed"),
     "bricks": PbrProperties.from_gpuopen("TH Large Red Bricks").scale(2, 2),
-    # "alu_matte": PbrProperties.from_gpuopen("Aluminum Matte").scale(2, 2),
     "leather": PbrProperties.from_gpuopen("TH Brown Fabric Leather"),
     "alu_corr": PbrProperties.from_gpuopen("Aluminum Corrugated").scale(2, 2),
     "alu_hexagon": PbrProperties.from_gpuopen("Aluminum Hexagon").scale(4, 4),
@@ -45,29 +45,32 @@ materials = {
     "brass_scratched": PbrProperties.from_ambientcg("Metal 007"),
     "carbon": PbrProperties.from_ambientcg("Fabric 004").scale(2, 2),
     "plates": PbrProperties.from_ambientcg("Metal Plates 006"),
-    "acryl": PbrProperties.from_physicallybased("Plastic (Acrylic)"),
+    "floor": PbrProperties.from_gpuopen("Adelie Brown Luxury Flooring").scale(4, 4),
     "plank": PbrProperties.from_polyhaven("Plank Flooring 03"),
     "rock_wall": PbrProperties.from_polyhaven("Rock Wall 16").scale(5, 5),
 }
-print()
+
 # %%
 
 
 shader_ball = create_shader_ball("shader_ball")
-shader_ball.material = materials["tiles2"]
 show(
     shader_ball,
+    Pos(20, 0, 0) * shader_ball,
+    materials=[materials["floor"], materials["steel"]],
     studio_texture_mapping=StudioTextureMapping.PARAMETRIC,
     reset_camera=Camera.ISO,
 )
 set_viewer_config(tab="studio")
 
-export_gltf(shader_ball, "shader_ball.glb")
+# export_gltf(shader_ball, "shader_ball.glb")
 
 # %%
 shader_ball = create_shader_ball("shader_ball")
-objects = []
+
 n = 5
+material_matrix = []
+shader_balls = []
 for i in range(n):
     for j in range(n):
         material_name = list(materials.keys())[i * 5 + j]
@@ -75,16 +78,14 @@ for i in range(n):
         sb.label = f"ball_{material_name}"
 
         material = materials[material_name]
-        sb.material = material
+        sb.color = material.interpolate_color()
 
-        objects.append(sb)
-
-all_sb = Compound(label="shader_balls", children=objects)
+        material_matrix.append(material)
+        shader_balls.append(sb)
 
 show(
-    all_sb,
+    *shader_balls,
+    materials=material_matrix,
     reset_camera=Camera.ISO,
 )
 set_viewer_config(tab="studio")
-
-export_gltf(all_sb, "shader_balls.glb")
